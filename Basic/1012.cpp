e499a3a922732d99c434be5b0a1660a7ea2ae2d6
lihaocheng <lihaocheng00@126.com>
2019-11-01 10:23:49 +0800

    1012

--- Basic/1012.cpp
+++ Basic/1012.cpp
@@ -3,39 +3,42 @@
 //
 #include <stdio.h>
 int main(void){
-    int result[5];
-    int flag=1;
+    int result[5]={0};
+    int flag=1,flag2=0;
     int count=0;
-    for(int i=0;i<=5;i++){
-        result[i]=0;
-    }
+
     int n;
     int a;
     scanf("%d",&n);
     for(int i=0;i<n;i++){
         scanf("%d",&a);
         if(a%5==0&&a%2==0){
-            result[1]+=a;
+            result[0]+=a;
         } else if(a%5==1){
+            //这里使用一个flag2来做标记，因为若a1-a2等于0，按题目要求应该输出0，而不是N
+            flag2=1;
             if(flag){
-                result[2]+=a;
+                result[1]+=a;
                 flag=0;
             } else {
-                result[2]-=a;
+                result[1]-=a;
                 flag=1;
             }
         }else if(a%5==2){
-            result[3]++;
+            result[2]++;
         } else if(a%5==3){
-            result[4]+=a;
+            result[3]+=a;
             count++;
         } else if(a%5==4){
-            if(a>result[5]){
-                result[5]=a;
+            if(a>result[4]){
+                result[4]=a;
             }
         }
     }
-    if(result[1]==0){
+    if(result[0]==0){
+        printf("N ");
+    } else printf("%d ",result[0]);
+    if(flag2==0){
         printf("N ");
     } else printf("%d ",result[1]);
     if(result[2]==0){
@@ -43,13 +46,10 @@ int main(void){
     } else printf("%d ",result[2]);
     if(result[3]==0){
         printf("N ");
-    } else printf("%d ",result[3]);
+    } else printf("%.1f ",(double)result[3]/count);
     if(result[4]==0){
-        printf("N ");
-    } else printf("%.1f ",(double)result[4]/count);
-    if(result[5]==0){
         printf("N");
-    } else printf("%d",result[5]);
+    } else printf("%d",result[4]);
 
     return 0;
 }
