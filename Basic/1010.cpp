35a5cfcf781e1e34f3e1f013401476f10ac5ba3d
lihaocheng <lihaocheng00@126.com>
2019-11-05 23:49:32 +0800

    1010

--- Basic/1010.cpp
+++ Basic/1010.cpp
@@ -2,8 +2,35 @@
 // Created by 李昊城 on 2019/11/3.
 //
 #include <stdio.h>
+#include <cstdio>
+
 int main(void){
     int a[100];
-    int k,e
-    while (scanf("%d%d",))
+    int i=0;
+    int j,k;
+    int count;
+    while (scanf("%d%d",&j,&k)==2){
+        a[k]=j;
+        if(j>i)i=j;
+    }
+    a[0]=0;
+    for(int n=1;n<i;n++){
+        a[n-1]=a[n]*n;
+        a[n]=0;
+        if(a[i-1]!=0)count++;
+    }
+    if(count==0){
+        printf("0 0");
+    } else {
+        for (int n = i; n > 0; n--) {
+            if (a[n] != 0) {
+                printf("%d ", a[n]);
+                count--;
+                if (count != 0)printf(" ");
+            }
+
+        }
+    }
+    return 0;
+
 }
