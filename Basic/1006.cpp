fe2189e0ffe10c329963068206e1befc500e5080
lihaocheng <lihaocheng00@126.com>
2019-11-16 23:58:49 +0800

    1006init

---
+++ Basic/1006.cpp
@@ -0,0 +1,21 @@
+//
+// Created by 李昊城 on 2019/11/16.
+//
+#include <stdio.h>
+int main(void){
+    int a,i=0;
+    int b[10]={0};
+    scanf("%d",&a);
+    do{
+        b[i]=a;
+        a=a/10;
+        i++;
+    }while (a>9);
+    for(int i=0;i<10;i++){
+        for(i)
+    }
+    for(int i=0;i<=a;i++){
+        printf("%d",i);
+    }
+    return 0;
+}
