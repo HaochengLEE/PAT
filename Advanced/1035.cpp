0a0e17d30fd7a7ca7a8444108f69ece7bc8be4f9
lihaocheng <lihaocheng00@126.com>
2019-11-26 00:43:03 +0800

    1035

--- Advanced/1035.cpp
+++ Advanced/1035.cpp
@@ -3,64 +3,66 @@
 //
 #include <stdio.h>
 #include <string.h>
-
+struct User{
+    char strA[20];
+    char strB[20];
+};
 
 int main(void){
-    int num,count=0;
-    char strA[20],strB[20];
-    char endA[1010][20],endB[1010][20];
+    //count标记不需要修改的值，countB标记需要修改的值
+    int num,count=0,countB=0;
+    User std;
+    User end[1010];
     int k=0;
     scanf("%d",&num);
 
     while (num--){
-        scanf("%s %s",strA,strB);
-        int len=strlen(strB);
+        scanf("%s %s",std.strA,std.strB);
+        int len=strlen(std.strB);
         int flag=0;
         //遍历第二个字符串
         for(int i=0;i<len;i++){
             //replace 1 (one) by @, 0 (zero) by %, l by L, and O by o
-            if(strB[i]=='1'){
-                strB[i]=='@';
+            if(std.strB[i]=='1'){
+                std.strB[i]='@';
                 flag=1;
-            } else if(strB[i]=='0'){
-                strB[i]='%';
+            } else if(std.strB[i]=='0'){
+                std.strB[i]='%';
                 flag=1;
-            } else if(strB[i]=='l'){
-                strB[i]='L';
+            } else if(std.strB[i]=='l'){
+                std.strB[i]='L';
                 flag=1;
-            } else if(strB[i]=='0'){
-                strB[i]='o';
+            } else if(std.strB[i]=='O'){
+                std.strB[i]='o';
                 flag=1;
             }
 
-            if(flag==0){
-                count++;
-            } else {
-                //存入新数组
-                endA[k++]=strA;
-                endB[k++]=strB;
-            }
+
+        }
+        if(flag==0){
+            //不需要修改的字符串个数
+            count++;
+        } else {
+            //存入新数组
+            end[k]=std;
+            k++;
+            //修改的字符串个数
+            countB++;
         }
     }
-    if(count==num){
+    if(countB==0){
         if(count==1){
-            printf("There is %d account and no account is modified",count);
+            printf("There is 1 account and no account is modified\n");
         } else{
-            printf("There are %d account and no account is modified",count);
+            printf("There are %d accounts and no account is modified\n",count);
         }
 
     } else{
-        printf("%d",num-count);
-        for(int i=0;i<num-count;i++){
-            printf("%s %s",endA[i],endB[i]);
+        printf("%d\n",countB);
+        for(int i=0;i<=countB;i++){
+            printf("%s %s\n",end[i].strA,end[i].strB);
         }
-        if(count>0){
-            if(count==1){
-                printf("There is %d account and no account is modified",count);
-            } else{
-                printf("There are %d account and no account is modified",count);
-            }
-        }
+
     }
 
 
